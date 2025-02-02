d1d8fc3d281ac02fd612675c25f7f2d274fc4952
James Z <125828382+Jamziewebdev@users.noreply.github.com>
2025-02-02 11:41:28 +0100

    Add project files.

---
+++ TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
@@ -0,0 +1,43 @@
+namespace TicketBookingCore.Tests
+{
+    public class TicketBookingRequestProcessorTests
+    {
+        private readonly TicketBookingRequestProcessor _processor;
+        public TicketBookingRequestProcessorTests()
+        {
+            _processor = new TicketBookingRequestProcessor();
+        }
+
+        [Fact]
+        public void ShouldReturnTicketBookingResultWithRequestValues()
+        {
+            // Arrange
+            var request = new TicketBookingRequest
+            {
+                FirstName = "Jamie",
+                LastName = "Ziegler",
+                Email = "fake@mail.com"
+            };
+
+            // Act
+            TicketBookingResponse response = _processor.Book(request);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, response.Email);
+        }
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            // Arrange
+            var processor = new TicketBookingRequestProcessor();
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => processor.Book(null));
+            // Assert
+            Assert.Equal("request", exception.ParamName);
+        }
+
+    }
+}
---
+++ TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -0,0 +1,24 @@
+﻿namespace TicketBookingCore
+{
+    public class TicketBookingRequestProcessor
+    {
+        public TicketBookingRequestProcessor()
+        {
+        }
+
+        public TicketBookingResponse Book(TicketBookingRequest request)
+        {
+            if (request is null)
+            {
+                throw new System.ArgumentNullException(nameof(request));
+            }
+
+            return new TicketBookingResponse
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            };
+        }
+    }
+}
